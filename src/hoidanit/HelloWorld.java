777e52962f363450374d60861761f8aec8b33763
Ta Van Cuong <tavancuong4444@gmail.com>
2024-12-10 23:02:06 +0700

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,10 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and HOIDANIT");
+		System.out.println("Hello world with java and HOIDANIT");
+		System.out.println("Hello world with java and HOIDANIT");
+		System.out.println("Hello world with java and HOIDANIT");
+	}
+}
